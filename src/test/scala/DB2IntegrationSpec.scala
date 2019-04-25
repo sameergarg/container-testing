498efd734e4ec9807857866ff17c9c4cfeb2a393
samirgarg <samir.garg@deloitte.co.uk>
2019-04-25 14:55:53 +0100

    Add ability to execute tests in parallel

--- src/test/scala/DB2IntegrationSpec.scala
+++ src/test/scala/DB2IntegrationSpec.scala
@@ -1,19 +1,19 @@
 import com.whisk.docker.scalatest.DockerTestKit
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{Matchers, WordSpec}
 import slick.jdbc.PostgresProfile.api._
-import monix.eval.Task
-import monix.execution.Scheduler.Implicits.global
 
-class DBIntegrationSpec extends WordSpec with Matchers with DockerTestKit with DockerPostgresSetup
+class DB2IntegrationSpec extends WordSpec with Matchers with DockerTestKit with DockerPostgresSetup
   with ScalaFutures {
 
   implicit val defaultPatience: PatienceConfig =
     PatienceConfig(timeout =  Span(20, Seconds), interval = Span(5, Millis))
 
   "DB connection" must {
-    "be obtained from container" in {
+    "be obtained from container" in new {
       val eventualCoffees: Task[Seq[Coffee]] = for {
         _       <- runFlywayMigration
         coffees <- Task.deferFuture(db.run(Coffees.coffees.result.map {
--- src/test/scala/DockerPostgresSetup.scala
+++ src/test/scala/DockerPostgresSetup.scala
@@ -9,19 +9,18 @@ import org.flywaydb.core.Flyway
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
-import scala.util.Try
+import scala.util.{Random, Try}
 
-trait DBConfig {
-  val hostPort = 44444
+trait DockerPostgresSetup extends DockerKit {
+  lazy val hostPort = 2000 + Random.nextInt(8000)
   val containerPort = 5432
   val user = "postgres"
   val password = "safepassword"
   val database = "postgres" //same as user
   val driver = "org.postgresql.Driver"
   val dockerImage = "postgres:10.4"
-}
+  lazy val dbUrl = s"jdbc:postgresql://localhost:$hostPort/$database?ssl=false"
 
-trait DockerPostgresSetup extends DockerKit with DBConfig {
 
   private lazy val client: DockerClient = DefaultDockerClient.fromEnv().build()
   override implicit lazy val dockerFactory: DockerFactory = new SpotifyDockerFactory(client)
@@ -29,32 +28,11 @@ trait DockerPostgresSetup extends DockerKit with DBConfig {
   lazy val postgresContainer: DockerContainer = DockerContainer(dockerImage)
     .withPorts((containerPort, Some(hostPort)))
     .withEnv(s"POSTGRES_USER=$user", s"POSTGRES_PASSWORD=$password", s"POSTGRES_DB=$database")
-    .withReadyChecker(new PostgresReadyChecker().looped(10, 1.second))
+    .withReadyChecker(new PostgresReadyChecker(dbUrl, user, password, driver).looped(10, 1.second))
 
   // adds our container to the DockerKit's list
   abstract override def dockerContainers: List[DockerContainer] =
     postgresContainer :: super.dockerContainers
-}
-
-class PostgresReadyChecker extends DockerReadyChecker with DBConfig {
-  override def apply(container: DockerContainerState
-                    )(implicit docker: DockerCommandExecutor, ec: ExecutionContext) : Future[Boolean] = {
-
-    container.getPorts().map { ports =>
-      val url = s"jdbc:postgresql://localhost:$hostPort/$database?ssl=false"
-      Try {
-        Class.forName("org.postgresql.Driver")
-        Option(DriverManager.getConnection(url, user, password))
-          .map(_.close)
-          .isDefined
-      }.getOrElse(false)
-    }
-  }
-}
-
-trait InitDockerDB extends DBConfig {
-
-  private lazy val dbUrl = s"jdbc:postgresql://localhost:$hostPort/$database?ssl=false"
 
   private lazy val config = {
     val hConfig = new HikariConfig
@@ -79,7 +57,22 @@ trait InitDockerDB extends DBConfig {
   import slick.jdbc.PostgresProfile.api._
 
   lazy val db = Database.forDataSource(dataSource, None)
+
 }
 
+class PostgresReadyChecker(dbUrl: String, user: String, password: String, driver: String) extends DockerReadyChecker {
+  override def apply(container: DockerContainerState
+                    )(implicit docker: DockerCommandExecutor, ec: ExecutionContext) : Future[Boolean] = {
+
+    container.getPorts().map { _ =>
+      Try {
+        Class.forName(driver)
+        Option(DriverManager.getConnection(dbUrl, user, password))
+          .map(_.close)
+          .isDefined
+      }.getOrElse(false)
+    }
+  }
+}
 
 
